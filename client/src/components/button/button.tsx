d04aef96a14b4a155356ce280bc6be3c37950058
EugeneNail <eugene.a.nail@gmail.com>
2023-10-28 17:30:16 +0000

    Add a custom button component

---
+++ client/src/components/button/button.tsx
@@ -0,0 +1,38 @@
+import { FC } from "react";
+import "./button.sass";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { IconDefinition, faEye, faLock } from "@fortawesome/free-solid-svg-icons";
+import { MouseEvent } from "react";
+
+type ButtonProps = {
+  text?: string;
+  type: "regular" | "light";
+  iconOnly?: boolean;
+  wide?: boolean;
+  leadingIcon?: IconDefinition;
+  trailingIcon?: IconDefinition;
+  action: () => void;
+};
+
+const Button: FC<ButtonProps> = (props) => {
+  let style = "button" + " button_" + props.type;
+  if (props.iconOnly) style += " button_icon-only";
+  if (props.wide) style += " button_wide";
+
+  const handleClick = (event: MouseEvent) => {
+    event.preventDefault();
+    props.action();
+  };
+
+  return (
+    <button onClick={handleClick} className={style}>
+      {props.leadingIcon && <FontAwesomeIcon className="button__leading-icon" icon={props.leadingIcon} />}
+      {!props.iconOnly && props.text}
+      {!props.iconOnly && props.trailingIcon && (
+        <FontAwesomeIcon className="button__trailing-icon" icon={props.trailingIcon} />
+      )}
+    </button>
+  );
+};
+
+export default Button;
--- client/src/root-router.tsx
+++ client/src/root-router.tsx
@@ -2,21 +2,25 @@ import { createBrowserRouter } from "react-router-dom";
 import GuestLayout from "./layouts/guest-layout/guest-layout";
 import LoginPage from "./pages/guest/login-page";
 import SignupPage from "./pages/guest/signup-page";
+import DefaultLayout from "./layouts/default-layout/default-layout";
 
 const rootRouter = createBrowserRouter([
   {
-    path: "/",
     element: <GuestLayout />,
     children: [
       {
-        path: "login",
+        path: "/login",
         element: <LoginPage />,
       },
       {
-        path: "signup",
+        path: "/signup",
         element: <SignupPage />,
       },
     ],
+  },
+  {
+    path: "/",
+    element: <DefaultLayout />,
   },
 ]);
 
