c52c4235d1ffe539f97b7d2928fcb385e2cde5ce
EugeneNail <eugene.a.nail@gmail.com>
2023-11-03 19:04:39 +0300

    Add a useMultiStepForm hook

---
+++ client/src/service/use-multi-step-form.tsx
@@ -0,0 +1,27 @@
+import { ReactElement, useState } from "react";
+
+function useMultiStepForm(steps: ReactElement[]) {
+  const [currentStep, setCurrentStep] = useState(0);
+
+  function back() {
+    setCurrentStep(currentStep > 0 ? currentStep - 1 : currentStep);
+  }
+
+  function next() {
+    setCurrentStep(currentStep < steps.length - 1 ? currentStep + 1 : currentStep);
+  }
+
+  function goTo(index: number) {
+    setCurrentStep(index >= 0 && index < steps.length - 1 ? index : 0);
+  }
+
+  return {
+    currentStep,
+    back,
+    next,
+    goTo,
+    steps,
+  };
+}
+
+export default useMultiStepForm;
